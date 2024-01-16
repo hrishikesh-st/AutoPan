7ac8573f0502ffaa1a7b3fed2dcc4ee869e89157
hrishikesh-st <hpawar@wpi.edu>
2024-01-16 16:00:25 -0500

    Refactoring image reading and corner detection to functions.

--- Phase1/Code/Wrapper.py
+++ Phase1/Code/Wrapper.py
@@ -22,6 +22,80 @@ import os
 import os.path as osp
 import argparse
 import distutils.util
+from skimage.feature import peak_local_max
+
+
+def readImages(data_dir, train=True, **kwargs):
+    """
+    Read a set of images for Panorama stitching
+
+    :param data_dir: DataSet directory
+    :type data_dir: str
+    :param train: Select Train or Test set, defaults to True
+    :type train: bool, optional
+    :return: List of images, format: [[image_name, original_image, image_grayscale], ...]
+    :rtype: list
+    """
+    images = []
+
+    image_path = osp.join(data_dir, "Train" if train else "Test", kwargs["ImageSet"])
+
+    for image_name in os.listdir(image_path):
+        image = cv2.imread(osp.join(image_path, image_name))
+        if image is not None:
+            image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            images.append([osp.join(image_path, image_name), image, image_grayscale.astype(np.float32)])
+
+    return images
+
+def detectCorners(image, results_dir_path, method="Harris", plot=True, **kwargs):
+    """
+    Corner Detection with Harris or Shi-Tomasi
+
+    :param image: list of [image_path, image, image_grayscale]
+    :type image: list
+    :param results_dir_path: Path to save the results
+    :type results_dir_path: str
+    :param method: Detection method, defaults to "Harris"
+    :type method: str, optional
+    :param plot: Flag for plotting, defaults to True
+    :type plot: bool, optional
+    :return: Corner detection output
+    :rtype: np.ndarray
+    """
+    image_name = image[0].split("/")[-1]
+    if method == "Harris":
+        print("Detecting corners with Harris... for image: ", image_name)
+        # Apply Harris Corner Detection
+        corner_det_image = cv2.cornerHarris(image[2], blockSize=7, ksize=11, k=0.04)
+        corner_det_image = cv2.dilate(corner_det_image, None)
+
+        if plot:
+            image[1][corner_det_image > 0.01 * corner_det_image.max()] = [0, 0, 255]
+
+            # Save the corner detection output
+            corner_image_path = osp.join(results_dir_path, f"corners_harris_{image_name}")
+            cv2.imwrite(corner_image_path, image[1])
+
+        return corner_det_image
+
+    elif method == "ShiTomasi":
+        print("Detecting corners with Shi-Tomasi... for image: ", image_name)
+        num_corners = kwargs["NumFeatures"]
+        corners = cv2.goodFeaturesToTrack(image[2], num_corners, 0.01, 10)
+
+        if plot:
+            if corners is not None:
+                    # Draw corners on the original image
+                    for i in corners:
+                        x, y = i.ravel()
+                        cv2.circle(image[1], (int(x), int(y)), 3, (0, 255, 0), -1)  # Ensure coordinates are integers
+
+            # Save the Shi-Tomasi corner detection output
+            shi_tomasi_image_path = osp.join(results_dir_path, f"corners_shi_tomasi_{image_name}")
+            cv2.imwrite(shi_tomasi_image_path, image[1])
+
+        return corners
 
 
 def main():
@@ -31,7 +105,6 @@ def main():
     Parser.add_argument('--SelectTrain', type=lambda x: bool(distutils.util.strtobool(x)), default=True, help='Choose the set to run the test on, Default:True')
     Parser.add_argument('--ImageSet', default="Set1", help='Choose the set to run the test on Options are Set1, Set2, Set3, CustomSet1, CustomSet2, Default:Set1')
     Parser.add_argument('--SelectDetector', default="Harris", help='Choose the detector to use Options are Harris, ShiTomasi, Default:Harris')
-    
 
     Args = Parser.parse_args()
     NumFeatures = Args.NumFeatures
@@ -44,76 +117,21 @@ def main():
     """
     # Initialize the list of images
     DATA = "../Data"
-    images = []
-
-	# Initialize the flag for data selection and select set of images
-    # train = True
-    # image_set = "Set1" # Options are Set1, Set2, Set3, CustomSet1, CustomSet2
-    image_path = osp.join(DATA, "Train" if SelectTrain else "Test", ImageSet)
 
     # Create directory to save the results
     results_dir_path = osp.join(DATA, "Results")
     if not osp.exists(results_dir_path):
         os.makedirs(results_dir_path)
 
-	# Read and save data in grayscale
-    for image_name in os.listdir(image_path):
-        # Read image
-        image = cv2.imread(osp.join(image_path, image_name))
-        if image is not None:
-            image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            images.append([image_name, image_grayscale.astype(np.float32)])
+    images = readImages(DATA, SelectTrain, ImageSet=ImageSet)
 
     """
 	Corner Detection
 	Save Corner detection output as corners.png
 	"""
     # Sanity check
-    for image in images: # image is a list of [image_name, image_grayscale]
-
-        og_image = cv2.imread(osp.join(image_path, image[0]))
-        """"    
-        # Sanity Checks
-        print(f"Shape for {image[0]} is {image[1].shape}")
-        print(f"Type for {image[0]} is {type(image[1])} with dtype {image[1].dtype}")
-        print("*" * 33)
-        """
-        if SelectDetector == "Harris":
-            print("Detecting corners using Harris Corner Detector")
-            # Apply Harris Corner Detection
-            corner_det_image = cv2.cornerHarris(image[1], blockSize=7, ksize=11, k=0.04)
-            
-            """
-            # Save the corner detection output for visualization
-            cv2.imwrite(osp.join(results_dir_path, f"corners_{image[0]}"), corner_det_image)
-            """
-
-            corner_det_image = cv2.dilate(corner_det_image, None)
-
-            # Read the original image for plotting corners
-            og_image[corner_det_image > 0.01 * corner_det_image.max()] = [0, 0, 255]
-
-            # Save the corner detection output
-            corner_image_path = osp.join(results_dir_path, f"corners_harris_{image[0]}")
-            cv2.imwrite(corner_image_path, og_image)
-
-
-        elif SelectDetector == "ShiTomasi":
-            print("Detecting corners using Shi-Tomasi Corner Detector")
-            # Shi-Tomasi Corner Detection
-            # Specify the number of corners, Default: 100
-            num_corners = NumFeatures 
-            corners = cv2.goodFeaturesToTrack(image[1], num_corners, 0.01, 10)
-
-            if corners is not None:
-                # Draw corners on the original image
-                for i in corners:
-                    x, y = i.ravel()
-                    cv2.circle(og_image, (int(x), int(y)), 3, (0, 255, 0), -1)  # Ensure coordinates are integers
-
-            # Save the Shi-Tomasi corner detection output
-            shi_tomasi_image_path = osp.join(results_dir_path, f"corners_shi_tomasi_{image[0]}")
-            cv2.imwrite(shi_tomasi_image_path, og_image)
+    for image in images: # image is a list of [image_path, image, image_grayscale]
+        corner_det_image = detectCorners(image, results_dir_path, method=SelectDetector, plot=True, NumFeatures=NumFeatures)
 
 
     """
